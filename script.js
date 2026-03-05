be8f5b8763f17dde78257d4f48e352ada6f7d217
Shaquan Legae <shaquanlegae@gmail.com>
2026-03-05 19:54:07 +0200

    Revert changes for mobile view

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
+// Active sidebar button switching
 document.querySelectorAll('.aside-btn').forEach(btn => {
     btn.addEventListener('click', () => {
         document.querySelectorAll('.aside-btn').forEach(b => b.classList.remove('active'));
         btn.classList.add('active');
     });
 });
-
