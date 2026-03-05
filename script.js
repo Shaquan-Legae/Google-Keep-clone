a44d7383b7ba0c890472ae8e3355c7c75e8aaddc
Shaquan Legae <shaquanlegae@gmail.com>
2026-03-05 19:51:45 +0200

    Added layout improvements

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+document.querySelectorAll('.aside-btn').forEach(btn => {
+    btn.addEventListener('click', () => {
+        document.querySelectorAll('.aside-btn').forEach(b => b.classList.remove('active'));
+        btn.classList.add('active');
+    });
+});
 
